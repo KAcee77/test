233db159cd5498336cb25821ae22a162783ec6ad
Yaroslav <benyros@mail.ru>
2021-11-15 15:49:49 +0400

    fix output name filter

--- api/company/filters.py
+++ api/company/filters.py
@@ -3,8 +3,8 @@ from company.models import Employee
 
 
 class EmployeeFilter(django_filters.FilterSet):
-    department_id = django_filters.CharFilter(field_name='department__pk', lookup_expr='exact', required=False)
-    last_name = django_filters.CharFilter(field_name='last_name', lookup_expr='iexact', required=False)
+    department_id = django_filters.CharFilter(lookup_expr='exact', required=False)
+    last_name = django_filters.CharFilter(lookup_expr='iexact', required=False)
 
     class Meta:
         model = Employee
